bfca7c29ddfe27f944b7c90c928f002817917c3e
Farhan Alam <farhanalam.dev@outlook.com>
2022-11-12 09:37:17 +0000

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,43 @@
+//for (let i = 0; i <= 10; i++) {
+//  console.log(i, "Hello");
+//}
+//function greeting(name) {
+  //return console.log("Hello ", name);
+//}
+//greeting("Farhan");
+
+var oneLinerJoke = require('one-liner-joke');
+
+/*
+The variable getRandomJoke will contain a random joke with a format:
+{"body":"Artificial intelligence is no match for natural stupidity.","tags":["intelligence","stupid"]}
+*/
+var getRandomJoke = oneLinerJoke.getRandomJoke();
+console.log(getRandomJoke)
+
+/*
+One can add exclusion filter for the jokes tags
+default is ['racist', 'dirty', 'sex']
+*/
+var getRandomJoke = oneLinerJoke.getRandomJoke({
+    'exclude_tags': ['dirty', 'racist', 'marriage']
+  });
+console.log(getRandomJoke)
+
+
+/*
+The variable getRandomJoke will contain a random joke with a tag and with a format:
+{"body":"Artificial intelligence is no match for natural stupidity.","tags":["intelligence","stupid"]}
+*/
+
+var getRandomJokeWithTag = oneLinerJoke.getRandomJokeWithTag('stupid');
+console.log(getRandomJokeWithTag)
+
+/*
+One can add exclusion filter for the jokes tags
+default is ['racist', 'dirty', 'sex']
+*/
+var getRandomJoke = oneLinerJoke.getRandomJokeWithTag('stupid', {
+  'exclude_tags': ['dirty', 'racist', 'marriage']
+});
+console.log(getRandomJoke)
